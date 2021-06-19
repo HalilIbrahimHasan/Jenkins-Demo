d49d259955595807a2f6a0d1663f3c0ff3963f74
Ibrahim Kalin <hisetkal@gmail.com>
2021-06-19 04:39:49 -0400

    first initial commit

---
+++ src/test/java/com/techproed/FirstTest.java
@@ -0,0 +1,29 @@
+package com.techproed;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class FirstTest {
+
+
+    @Test
+    public void OpenBrowser()  {
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("disable-gpu");
+        driver = new ChromeDriver(options);
+        driver.get("https://gmibank.com/account/register");
+        System.out.println(driver.getTitle());
+        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("GMIBANK"));
+        System.out.println("This browser is navigating to "+driver.getTitle());
+    }
+
+
+}
+
+
