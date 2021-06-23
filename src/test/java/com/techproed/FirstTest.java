bb93e06b3ed8e8e7b2932335a8dca36376034ee4
Ibrahim Kalin <hisetkal@gmail.com>
2021-06-23 18:32:45 -0400

    all amazon items

--- src/test/java/com/techproed/FirstTest.java
+++ src/test/java/com/techproed/FirstTest.java
@@ -5,8 +5,11 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
 
 public class FirstTest {
 
@@ -21,7 +24,10 @@ public class FirstTest {
         driver = new ChromeDriver(options);
         driver.get("https://www.amazon.com");
         System.out.println("Title of the page is: " + driver.getTitle());
-
+        List <WebElement> allItems = driver.findElements(By.xpath("//header/div/div[4]/div[2]/div[2]/div/script/following::a"));
+        for (int i = 0; i < 8; i++) {
+            System.out.println("item "+(i+1)+" => "+allItems.get(i).getText());
+        }
 //        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
     }
 
