118a8fe6f12e5f1def59d6f3f5c505b9e544481a
Ibrahim Kalin <hisetkal@gmail.com>
2021-06-24 15:41:11 -0400

    latest

--- src/test/java/com/techproed/FirstTest.java
+++ src/test/java/com/techproed/FirstTest.java
@@ -14,19 +14,20 @@ import java.util.List;
 public class FirstTest {
 
 
-    @Test
-    public void OpenBrowser()  {
-        WebDriver driver;
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");
-        options.addArguments("disable-gpu");
-        driver = new ChromeDriver(options);
-        driver.get("https://www.amazon.com");
-        System.out.println("Title of the page is: " + driver.getTitle());
 
-//        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
-    }
+        @Test
+        public void OpenBrowser()  {
+            WebDriver driver;
+            System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("headless");
+            options.addArguments("disable-gpu");
+            driver = new ChromeDriver(options);
+            driver.get("https://www.google.com");
+            System.out.println("Title of the page is: " + driver.getTitle());
+            Assert.assertTrue("This page is unexpected!",driver.getTitle().equals("Google"));
+        }
+
 
 
 }
